17ad4a73419544feee571b3832ec3a5c6fb73103
daughlas <daughlas@gmail.com>
2020-07-04 09:10:28 +0800

    move to github

--- leetcode/test/string/lession1.test.js
+++
@@ -1,5 +0,0 @@
-import revertByWord from '../../code/string/lession1'
-
-test("revertByWord: Let's take LeetCode contest", () => {
-  expect(revertByWord('Let\'s take LeetCode contest')).toBe('s\'teL ekat edoCteeL tsetnoc')
-})
